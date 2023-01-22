3fd2c18ca44343306470ade66915ab16effed47a
AmirM561 <amirmohammadghaderi1@gmail.com>
2023-01-22 21:47:23 +0330

    Statistics
    creating Statistics of Home page

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import CoursesComponent from "../../components/courses/CoursesComponent";
 import FooterComponents from "../../components/Footer/FooterComponents";
-
+import Statistics from "../../components/Statistics/StatisticsComponent";
 function HomePage() {
   return <>
-
+  <Statistics/>
   <CoursesComponent/>
   <FooterComponents/>
   </>;
