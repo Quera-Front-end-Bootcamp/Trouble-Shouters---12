b26231cd783af32711f3595a87198338d48d1bda
mahannajafi <mahan.nwork@gmail.com>
2023-01-30 22:49:23 +0330

    respansive product page

--- src/components/DesProduct/DesProduct.jsx
+++ src/components/DesProduct/DesProduct.jsx
@@ -3,6 +3,10 @@ import Burger from "../Navbar/Burger/Burger";
 import styled from "styled-components";
 import headerImg from "../../images/ProductPage/back.png";
 import js from "../../images/ProductPage/js.png";
+import teacher from "../../images/ProductPage/teacher.png";
+import like from "../../images/ProductPage/like.png";
+import calendar from "../../images/ProductPage/calendar.png";
+
 export const HeaderMain = styled.header`
   background-image: url(${headerImg});
   background-position: top center;
@@ -10,6 +14,9 @@ export const HeaderMain = styled.header`
   box-shadow: inset 0 0 0 1000px rgba(31, 30, 30, 0.7);
   width: 100%;
   height: 100vh;
+  @media (max-width: 480px) {
+    height: 900px;
+  }
 `;
 export const Container = styled.div`
   color: white;
@@ -30,11 +37,18 @@ export const Logo = styled.img`
   height: 120px;
   width: 100px;
   align-items: center;
+  @media (max-width: 480px) {
+    height: 100px;
+    width: 80px;
+  }
 `;
 export const Title = styled.div`
   font-size: 36px;
   padding: 10px 60px;
   border-bottom: 1px solid #838181;
+  @media (max-width: 480px) {
+    font-size: 28px;
+  }
 `;
 export const Numbers = styled.div`
   display: flex;
@@ -43,8 +57,16 @@ export const Numbers = styled.div`
   font-size: 20px;
   width: 100%;
 `;
-export const All = styled.div``;
-export const Number = styled.div``;
+export const All = styled.div`
+  @media (max-width: 480px) {
+    font-size: 18px;
+  }
+`;
+export const Number = styled.div`
+  @media (max-width: 480px) {
+    font-size: 18px;
+  }
+`;
 export const ProgressBar = styled.div`
   background-color: #dbdbdb;
   height: 40px;
@@ -58,6 +80,12 @@ export const ProgressBar = styled.div`
 export const Boxes = styled.div`
   display: flex;
   width: 100%;
+  @media (max-width: 480px) {
+  flex-direction: column;
+  margin-bottom: 55px;
+  justify-content: center;
+  align-items: center;
+  }
 `;
 export const Box = styled.div`
   width: 33.3%;
@@ -69,10 +97,25 @@ export const Box = styled.div`
   padding: 80px;
   margin-bottom: 44px;
   background-color: rgba(31, 30, 30, 0.7);
+  img {
+    height: 25px;
+    width: 25px;
+    margin-bottom: 5px;
+  }
+  @media (max-width: 480px) {
+  width: 80%;
+  height: 70px;
+  margin-bottom: 0;
+  }
 `;
 export const BoxLogo = styled.div``;
-export const Line1 = styled.div``;
-export const Line2 = styled.div``;
+export const Line = styled.div`
+  margin-top: 7px;
+  @media (max-width: 480px) {
+    margin-top: 3px;
+    font-size: 12px;
+  }
+`;
 
 const DesProduct = () => {
   return (
@@ -91,19 +134,19 @@ const DesProduct = () => {
           </Details>
           <Boxes>
             <Box>
-              <BoxLogo></BoxLogo>
-              <Line1>: مدرس</Line1>
-              <Line2>دکتر بحر العلومی</Line2>
+              <img src={teacher} alt="" />
+              <Line>: مدرس</Line>
+              <Line>دکتر بحر العلومی</Line>
             </Box>
             <Box>
-              <BoxLogo></BoxLogo>
-              <Line1>تاریخ شروع : 1401/1/1</Line1>
-              <Line2>تاریخ پایان : 1402/1/1</Line2>
+              <img src={calendar} alt="" />
+              <Line>تاریخ شروع : 1401/1/1</Line>
+              <Line>تاریخ پایان : 1402/1/1</Line>
             </Box>
             <Box>
-              <BoxLogo></BoxLogo>
-              <Line1>رتبه :</Line1>
-              <Line2></Line2>
+              <img src={like} alt="" />
+              <Line>رتبه :</Line>
+              <Line>12</Line>
             </Box>
           </Boxes>
         </Container>
